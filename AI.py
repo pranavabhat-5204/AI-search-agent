3f2cd41532b9f5388d6ed09e4bb1b58865c27ebb
pranavabhat-5204 <72594385+pranavabhat-5204@users.noreply.github.com>
2025-03-04 23:10:31 +0530

    Update AI.py

--- AI.py
+++ AI.py
@@ -7,10 +7,14 @@ tavily_client = TavilyClient(api_key="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"
 # Step 2. Executing the search request
 response = tavily_client.search(Query, search_depth = "advanced", max_results=5)
 # Step 3. Printing the search results
+urls=[]
+for result in response['results']:
+  url=result['url']
+  urls.append(url)
+response = tavily_client.extract(urls=urls, include_images=False)
 Information=''
 for result in response['results']:
-  content=result['content']
-  Information=Information+content
+  Information=Information+result['raw_content']
 print(Information)
 from langchain_openai import OpenAI
 from getpass import getpass

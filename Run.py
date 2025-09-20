f6faa1e539e6a6274c7f12584bb594216b58cfb3
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-09-20 22:07:40 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,1 +1,10 @@
+from pyngrok import ngrok
+import subprocess
+import time
 
+subprocess.Popen(["streamlit", "run", "app.py", "--server.port=8501", "--server.headless=true"])
+
+time.sleep(5)
+
+public_url = ngrok.connect(8501)
+print("Your Streamlit app is live at:", public_url)
